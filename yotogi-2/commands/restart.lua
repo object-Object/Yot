5f78a921a9090a712c6a9b45e7d9f9bef5487b96
[object Object] <37044997+object-Object@users.noreply.github.com>
2020-03-28 20:10:47 +0000

    command to restart bot

---
+++ yotogi-2/commands/restart.lua
@@ -0,0 +1,21 @@
+local commandHandler = require("../commandHandler")
+local utils = require("../miscUtils")
+
+return {
+	name = "restart",
+	description = "Restart the bot.",
+	usage = "restart",
+	visible = false,
+	permissions = {"bot.botOwner"},
+	run = function(self, message, argString, args, guildSettings, conn)
+		utils.sendEmbed(message.channel, "Restarting.", "00ff00")
+		os.exit()
+	end,
+	onEnable = function(self, message, guildSettings)
+		return true
+	end,
+	onDisable = function(self, message, guildSettings)
+		return true
+	end,
+	subcommands = {}
+}
