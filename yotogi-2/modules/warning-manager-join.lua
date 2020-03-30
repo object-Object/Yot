ab36fa7aea273d77d70e2a180c50dc1e7655d1f6
[object Object] <37044997+object-Object@users.noreply.github.com>
2020-03-30 20:17:02 +0000

    handle people leaving or joining with warnings

---
+++ yotogi-2/modules/warning-manager-join.lua
@@ -0,0 +1,36 @@
+local utils = require("../miscUtils")
+local commandHandler = require("../commandHandler")
+
+return {
+	name = "warning-manager-join",
+	description = "Set is_active to 1 and reset end_timestamp when users with warnings rejoin.",
+	visible = false,
+	event = "client.memberJoin",
+	disabledByDefault = false,
+	run = function(self, guildSettings, member, conn)
+		local entry, _ = conn:exec('SELECT * FROM warnings WHERE guild_id = "'..member.guild.id..'" AND user_id = "'..member.id..'";')
+		if entry then
+			entry = utils.formatRow(entry)
+			entry.is_active = true
+			entry.end_timestamp = os.time()+guildSettings.warning_length
+			conn:exec('UPDATE warnings SET is_active = 1, end_timestamp = '..entry.end_timestamp..' WHERE guild_id = "'..member.guild.id..'" AND user_id = "'..member.id..'";')
+			local warnFooter = commandHandler.strings.warnFooter(guildSettings, entry)
+			utils.sendEmbed(member:getPrivateChannel(), "Your warnings have been given back in **"..member.guild.name.."**. You have "..entry.level.." warning"..utils.s(entry.level)..".", "00ff00", warnFooter)
+			local publicLogChannel = guildSettings.public_log_channel and member.guild:getChannel(guildSettings.public_log_channel)
+			local staffLogChannel = guildSettings.staff_log_channel and member.guild:getChannel(guildSettings.public_log_channel)
+			if publicLogChannel then
+				utils.sendEmbed(staffLogChannel, name.."'s warnings have been given back. They have "..entry.level.." warning"..utils.s(entry.level)..".", "00ff00", warnFooter)
+			end
+			if staffLogChannel then
+				utils.sendEmbed(staffLogChannel, name.."'s warnings have been given back. They have "..entry.level.." warning"..utils.s(entry.level)..".", "00ff00", warnFooter)
+			end
+			utils.setGame(member.client, conn)
+		end
+	end,
+	onEnable = function(self, message, guildSettings, conn)
+		return true
+	end,
+	onDisable = function(self, message, guildSettings, conn)
+		return true
+	end
+}
---
+++ yotogi-2/modules/warning-manager-leave.lua
@@ -0,0 +1,22 @@
+local utils = require("../miscUtils")
+
+return {
+	name = "warning-manager-leave",
+	description = "Set is_active to 0 when users with warnings leave.",
+	visible = false,
+	event = "client.memberLeave",
+	disabledByDefault = false,
+	run = function(self, guildSettings, member, conn)
+		local entry, _ = conn:exec('SELECT * FROM warnings WHERE guild_id = "'..member.guild.id..'" AND user_id = "'..member.id..'";')
+		if entry then
+			conn:exec('UPDATE warnings SET is_active = 0 WHERE guild_id = "'..member.guild.id..'" AND user_id = "'..member.id..'";')
+			utils.setGame(member.client, conn)
+		end
+	end,
+	onEnable = function(self, message, guildSettings, conn)
+		return true
+	end,
+	onDisable = function(self, message, guildSettings, conn)
+		return true
+	end
+}
