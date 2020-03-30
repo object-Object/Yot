32dc37326189c1c59bf3cfb3e2a557fecd768366
[object Object] <37044997+object-Object@users.noreply.github.com>
2020-03-30 01:44:27 +0000

    check for false actives, and add onEnable and onDisable logic

--- yotogi-2/modules/warning-manager.lua
+++ yotogi-2/modules/warning-manager.lua
@@ -1,5 +1,7 @@
 local utils = require("../miscUtils")
 local commandHandler = require("../commandHandler")
+local moduleHandler = require("../moduleHandler")
+local options = require("../options")
 
 return {
 	name = "warning-manager",
@@ -12,33 +14,47 @@ return {
 		if warnings then
 			local decreaseStmt = conn:prepare("UPDATE warnings SET level = ?, end_timestamp = ? WHERE guild_id = ? AND user_id = ?;")
 			local deleteStmt = conn:prepare("DELETE FROM warnings WHERE guild_id = ? AND user_id = ?;")
+			local fixActiveStmt = conn:prepare("UPDATE warnings SET active = 0 WHERE guild_id = ? AND user_id = ?;")
 			for row=1, nrow do
 				local warnMember = guild:getMember(warnings.user_id[row])
-				local warnUser = guild.client:getUser(warnings.user_id[row])
-				local level = warnings.level[row]-1
-				local end_timestamp = level>0 and os.time()+guildSettings.warning_length or 0
-				if level==0 then
-					deleteStmt:reset():bind(guild.id, warnUser.id):step()
+				if not warnMember then
+					fixActiveStmt:reset():bind(guild.id, warnUser.id):step()
 				else
-					decreaseStmt:reset():bind(level, end_timestamp, guild.id, warnUser.id):step()
-				end
-				local staffLogChannel = guildSettings.staff_log_channel and guild:getChannel(guildSettings.staff_log_channel)
-				local warnFooter = commandHandler.strings.warnFooter(guildSettings, {is_active=true, end_timestamp=end_timestamp, level=level})
-				local name = warnMember and warnMember.name.."#"..warnUser.discriminator or warnUser.tag
-				utils.sendEmbed(warnUser:getPrivateChannel(), "You have been automatically unwarned in **"..guild.name.."**. You now have "..level.." warning"..utils.s(level)..".", "00ff00", warnFooter)
-				if staffLogChannel then
-					utils.sendEmbed(staffLogChannel, name.." has been automatically unwarned. They now have "..level.." warning"..utils.s(level)..".", "00ff00", warnFooter)
+					local warnUser = guild.client:getUser(warnings.user_id[row])
+					local level = warnings.level[row]-1
+					local end_timestamp = level>0 and os.time()+guildSettings.warning_length or 0
+					if level==0 then
+						deleteStmt:reset():bind(guild.id, warnUser.id):step()
+					else
+						decreaseStmt:reset():bind(level, end_timestamp, guild.id, warnUser.id):step()
+					end
+					local staffLogChannel = guildSettings.staff_log_channel and guild:getChannel(guildSettings.staff_log_channel)
+					local warnFooter = commandHandler.strings.warnFooter(guildSettings, {is_active=true, end_timestamp=end_timestamp, level=level})
+					local name = warnMember.name.."#"..warnUser.discriminator
+					utils.sendEmbed(warnUser:getPrivateChannel(), "You have been automatically unwarned in **"..guild.name.."**. You now have "..level.." warning"..utils.s(level)..".", "00ff00", warnFooter)
+					if staffLogChannel then
+						utils.sendEmbed(staffLogChannel, name.." has been automatically unwarned. They now have "..level.." warning"..utils.s(level)..".", "00ff00", warnFooter)
+					end
 				end
 			end
 			decreaseStmt:close()
 			deleteStmt:close()
+			fixActiveStmt:close()
 			utils.setGame(guild.client, conn)
 		end
 	end,
-	onEnable = function(self, message, guildSettings)
+	onEnable = function(self, message, guildSettings, conn)
+		moduleHandler.enable("warning-manager-join", message, guildSettings, conn)
+		moduleHandler.enable("warning-manager-leave", message, guildSettings, conn)
+		conn:exec("UPDATE guild_settings SET warning_length = "..options.warningLength.." WHERE guild_id = '"..message.guild.id.."';")
+		utils.sendEmbed(message.channel, "Warnings will now expire. The warning_length setting has been set to "..options.warningLength..".", "00ff00")
 		return true
 	end,
-	onDisable = function(self, message, guildSettings)
+	onDisable = function(self, message, guildSettings, conn)
+		moduleHandler.disable("warning-manager-join", message, guildSettings, conn)
+		moduleHandler.disable("warning-manager-leave", message, guildSettings, conn)
+		conn:exec("UPDATE guild_settings SET warning_length = 0 WHERE guild_id = '"..message.guild.id.."';")
+		utils.sendEmbed(message.channel, "Warnings will no longer expire. The warning_length setting has been set to 0.", "00ff00")
 		return true
 	end
 }
