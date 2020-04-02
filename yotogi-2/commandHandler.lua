b9cfc800f41c4dc19c56329378d209d4b3db1edd
[object Object] <37044997+object-Object@users.noreply.github.com>
2020-04-02 17:57:33 +0000

    make usageFooter more general

--- yotogi-2/commandHandler.lua
+++ yotogi-2/commandHandler.lua
@@ -15,7 +15,7 @@ commandHandler.customPermissions = {
 }
 
 commandHandler.strings = { -- bits of text used in multiple places that should be consistent
-	usageFooter = "Angled brackets represent required arguments. Square brackets represent optional arguments. Do not include the brackets in the command. Commands are case sensitive.",
+	usageFooter = "Angled brackets represent required arguments. Square brackets represent optional arguments. Do not include the brackets in the command. All values are case sensitive.",
 	warnFooter = function(guildSettings, entry)
 		return "Time until a warning is removed: "..(entry.is_active and utils.secondsToTime(entry.end_timestamp-os.time()) or "N/A").."\n"
 			.."Warnings until kick: "..(entry.level<guildSettings.warning_kick_level and guildSettings.warning_kick_level-entry.level or "N/A").."\n"
