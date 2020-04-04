c461b46aafa4a651ba88728b62f0e47f8e70d7ff
[object Object] <37044997+object-Object@users.noreply.github.com>
2020-04-04 16:57:40 +0000

    add ban messages

---
+++ yotogi-2/modules/ban-messages.lua
@@ -0,0 +1,19 @@
+local utils = require("../miscUtils")
+
+return {
+	name = "ban-messages",
+	description = "Send messages in the public log channel when a user is banned.",
+	visible = true,
+	event = "client.userBan",
+	disabledByDefault = true,
+	run = function(self, guildSettings, user, guild, conn)
+		local publicLogChannel = guildSettings.public_log_channel and guild:getChannel(guildSettings.public_log_channel)
+		utils.sendEmbedSafe(publicLogChannel, user.tag.." has been banned from this server.", "ffffff")
+	end,
+	onEnable = function(self, message, guildSettings, conn)
+		return true
+	end,
+	onDisable = function(self, message, guildSettings, conn)
+		return true
+	end
+}
