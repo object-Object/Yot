703d1622e4ffcdaf6b29feec5264010480feb64c
[object Object] <37044997+object-Object@users.noreply.github.com>
2020-04-04 03:50:11 +0000

    use the new muteUtils methods

--- yotogi-2/modules/mute-manager-join.lua
+++ yotogi-2/modules/mute-manager-join.lua
@@ -1,4 +1,5 @@
 local utils = require("../miscUtils")
+local muteUtils = require("../muteUtils")
 local commandHandler = require("../commandHandler")
 
 return {
@@ -7,49 +8,40 @@ return {
 	visible = false,
 	event = "client.memberJoin",
 	disabledByDefault = false,
-	run = function(self, guildSettings, member, conn)
-		local entry, _ = conn:exec('SELECT * FROM mutes WHERE guild_id = "'..member.guild.id..'" AND user_id = "'..member.id..'";')
-		if entry then
-			local staffLogChannel = guildSettings.staff_log_channel and member.guild:getChannel(guildSettings.staff_log_channel)
-			if not guildSettings.muted_role then
-				if staffLogChannel then
-					utils.sendEmbed(staffLogChannel, member.user.tag.."'s mute could not be given back because the `muted_role` setting is not set.", "ff0000")
-				end
-				return
-			end
-			local mutedRole = member.guild:getRole(guildSettings.muted_role)
-			if not mutedRole then
-				if staffLogChannel then
-					utils.sendEmbed(staffLogChannel, member.user.tag.."'s mute could not be given back because the role set as the `muted_role` setting no longer exists.", "ff0000")
-				end
-				return
-			end
-			local selfMember = member.guild:getMember(member.client.user.id)
-			if not selfMember:hasPermission("manageRoles") then
-				if staffLogChannel then
-					utils.sendEmbed(staffLogChannel, member.user.tag.."'s mute could not be given back because Yotogi does not have the `manageRoles` permission.", "ff0000")
-				end
-			elseif selfMember.highestRole.position<=mutedRole.position then
-				if staffLogChannel then
-					utils.sendEmbed(staffLogChannel, member.user.tag.."'s mute could not be given back because Yotogi's highest role is not higher than the role set as the `muted_role` setting.", "ff0000")
-				end
-			else
-				entry = utils.formatRow(entry)
-				entry.is_active = true
-				entry.end_timestamp = os.time()+entry.duration
-				conn:exec('UPDATE mutes SET is_active = 1, end_timestamp = '..entry.end_timestamp..' WHERE guild_id = "'..member.guild.id..'" AND user_id = "'..member.id..'";')
-				local muteFooter = commandHandler.strings.muteFooter(guildSettings, entry)
-				utils.sendEmbed(member:getPrivateChannel(), "Your mute has been given back in **"..member.guild.name.."**.", "00ff00", muteFooter)
-				local publicLogChannel = guildSettings.public_log_channel and member.guild:getChannel(guildSettings.public_log_channel)
-				if publicLogChannel then
-					utils.sendEmbed(publicLogChannel, member.user.tag.."'s mute has been given back.", "00ff00", muteFooter)
-				end
-				if staffLogChannel then
-					utils.sendEmbed(staffLogChannel, member.user.tag.."'s mute has been given back.", "00ff00", muteFooter)
-				end
-				member:addRole(mutedRole.id)
-			end
+	run = function(self, guildSettings, muteMember, conn)
+		local entry, _ = conn:exec('SELECT * FROM mutes WHERE guild_id = "'..muteMember.guild.id..'" AND user_id = "'..muteMember.id..'";')
+		if not entry then return end
+		entry = utils.formatRow(entry)
+
+		local muteUser = muteMember.user
+		local name = utils.name(muteUser, muteMember.guild)
+
+		local publicLogChannel = guildSettings.public_log_channel and muteMember.guild:getChannel(guildSettings.public_log_channel)
+		local staffLogChannel = guildSettings.staff_log_channel and muteMember.guild:getChannel(guildSettings.staff_log_channel)
+
+		local valid, reasonInvalid, mutedRole = muteUtils.checkValidMute(muteMember, muteUser, muteMember.guild, guildSettings)
+		if not valid then
+			local text = name.."'s mute could not be given back because "..reasonInvalid
+			utils.sendEmbedSafe(publicLogChannel, text, "ff0000")
+			utils.sendEmbedSafe(staffLogChannel, text, "ff0000")
+			muteUtils.deleteEntry(muteMember.guild, muteUser, conn)
+			return
 		end
+
+		local muteFooter = commandHandler.strings.muteFooter(guildSettings, entry.length, os.time()+entry.length, true)
+
+		local mutedDM = utils.sendEmbed(muteUser:getPrivateChannel(), "Your mute has been given back in **"..muteMember.guild.name.."**.", "00ff00", muteFooter)
+		local success, err = muteUtils.remute(muteMember, muteUser, mutedRole, muteMember.guild, conn, entry.length)
+		if not success then
+			if mutedDM then mutedDM:delete() end
+			local text = name.."'s mute could not be given back: `"..err.."`. Please report this error to the bot developer by sending Yotogi a direct message."
+			utils.sendEmbedSafe(publicLogChannel, text, "ff0000")
+			utils.sendEmbedSafe(staffLogChannel, text, "ff0000")
+			return
+		end
+		local text = name.."'s mute has been given back."
+		utils.sendEmbedSafe(publicLogChannel, text, "00ff00", muteFooter)
+		utils.sendEmbedSafe(staffLogChannel, text, "00ff00", muteFooter)
 	end,
 	onEnable = function(self, message, guildSettings, conn)
 		return true
