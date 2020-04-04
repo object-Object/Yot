9bcddd0e5c629cae221e72d6c599cf382675a183
[object Object] <37044997+object-Object@users.noreply.github.com>
2020-04-04 03:20:36 +0000

    drastically improve code structure and clarity

--- yotogi-2/modules/mute-manager.lua
+++ yotogi-2/modules/mute-manager.lua
@@ -2,6 +2,7 @@ local utils = require("../miscUtils")
 local commandHandler = require("../commandHandler")
 local moduleHandler = require("../moduleHandler")
 local options = require("../options")
+local muteUtils = require("../muteUtils")
 
 return {
 	name = "mute-manager",
@@ -11,69 +12,48 @@ return {
 	disabledByDefault = false,
 	run = function(self, guildSettings, guild, conn)
 		local mutes, nrow = conn:exec("SELECT * FROM mutes WHERE is_active = 1 AND end_timestamp <= "..os.time().." AND guild_id = '"..guild.id.."';")
-		if mutes then
-			local deleteStmt = conn:prepare("DELETE FROM mutes WHERE guild_id = ? AND user_id = ?;")
-			local fixActiveStmt = conn:prepare("UPDATE mutes SET is_active = 0 WHERE guild_id = ? AND user_id = ?;")
-			for row=1, nrow do
-				local muteMember = guild:getMember(mutes.user_id[row])
-				if not muteMember then
-					fixActiveStmt:reset():bind(guild.id, mutes.user_id[row]):step()
-				else
-					local muteUser = guild.client:getUser(mutes.user_id[row])
-					deleteStmt:reset():bind(guild.id, muteUser.id):step()
-					local name = muteMember.name.."#"..muteUser.discriminator
-					local publicLogChannel = guildSettings.public_log_channel and guild:getChannel(guildSettings.public_log_channel)
-					local staffLogChannel = guildSettings.staff_log_channel and guild:getChannel(guildSettings.staff_log_channel)
-					if not guildSettings.muted_role then
-						if publicLogChannel then
-							utils.sendEmbed(publicLogChannel, name.." could not be automatically unmuted because the `muted_role` setting is not set.", "ff0000")
-						end
-						if staffLogChannel then
-							utils.sendEmbed(staffLogChannel, name.." could not be automatically unmuted because the `muted_role` setting is not set.", "ff0000")
-						end
-					else
-						local mutedRole = guild:getRole(guildSettings.muted_role)
-						if not mutedRole then
-							if publicLogChannel then
-								utils.sendEmbed(publicLogChannel, name.." could not be automatically unmuted because the role set as the `muted_role` setting no longer exists.", "ff0000")
-							end
-							if staffLogChannel then
-								utils.sendEmbed(staffLogChannel, name.." could not be automatically unmuted because the role set as the `muted_role` setting no longer exists.", "ff0000")
-							end
-						else
-							if muteMember:hasRole(mutedRole.id) then
-								local selfMember = guild:getMember(guild.client.user.id)
-								if not selfMember:hasPermission("manageRoles") then
-									if publicLogChannel then
-										utils.sendEmbed(publicLogChannel, name.." could not be automatically unmuted because Yotogi does not have the `manageRoles` permission.", "ff0000")
-									end
-									if staffLogChannel then
-										utils.sendEmbed(staffLogChannel, name.." could not be automatically unmuted because Yotogi does not have the `manageRoles` permission.", "ff0000")
-									end
-								elseif selfMember.highestRole.position<=mutedRole.position then
-									if publicLogChannel then
-										utils.sendEmbed(publicLogChannel, name.." could not be automatically unmuted because Yotogi's highest role is not higher than the role set as the `muted_role` setting.", "ff0000")
-									end
-									if staffLogChannel then
-										utils.sendEmbed(staffLogChannel, name.." could not be automatically unmuted because Yotogi's highest role is not higher than the role set as the `muted_role` setting.", "ff0000")
-									end
-								else
-									utils.sendEmbed(muteUser:getPrivateChannel(), "You have been automatically unmuted in **"..guild.name.."**.", "00ff00")
-									if publicLogChannel then
-										utils.sendEmbed(publicLogChannel, name.." has been automatically unmuted.", "00ff00", nil, nil, muteUser.mentionString)
-									end
-									if staffLogChannel then
-										utils.sendEmbed(staffLogChannel, name.." has been automatically unmuted.", "00ff00")
-									end
-									muteMember:removeRole(mutedRole.id)
-								end
-							end
-						end
-					end
-				end
+		if not mutes then return end
+		for row=1, nrow do
+			local muteMember = guild:getMember(mutes.user_id[row])
+			if not muteMember then
+				conn:exec("UPDATE mutes SET is_active = 0 WHERE guild_id = '"..guild.id.."' AND user_id = '"..mutes.user_id[row].."';")
+				goto continue
 			end
-			deleteStmt:close()
-			fixActiveStmt:close()
+			local muteUser = muteMember.user
+			local name = utils.name(muteUser, guild)
+
+			local publicLogChannel = guildSettings.public_log_channel and guild:getChannel(guildSettings.public_log_channel)
+			local staffLogChannel = guildSettings.staff_log_channel and guild:getChannel(guildSettings.staff_log_channel)
+
+			local valid, reasonInvalid, mutedRole = muteUtils.checkValidMute(muteMember, muteUser, guild, guildSettings)
+			if not valid then
+				local text = name.." could not be automatically unmuted because "..reasonInvalid
+				utils.sendEmbedSafe(publicLogChannel, text, "ff0000")
+				utils.sendEmbedSafe(staffLogChannel, text, "ff0000")
+				muteUtils.deleteEntry(guild, muteUser, conn)
+				goto continue
+			end
+			local isMuted = muteUtils.checkIfMuted(muteMember, muteUser, mutedRole, guild, conn)
+			if not isMuted then
+				muteUtils.deleteEntry(guild, muteUser, conn)
+				goto continue
+			end
+
+			local mutedDM = utils.sendEmbed(muteUser:getPrivateChannel(), "You have been automatically unmuted in **"..guild.name.."**.", "00ff00")
+			local success, err = muteUtils.unmute(muteMember, muteUser, mutedRole, guild, conn)
+			if not success then
+				if mutedDM then mutedDM:delete() end
+				local text = name.." could not be automatically unmuted: `"..err.."`. Please report this error to the bot developer by sending Yotogi a direct message."
+				utils.sendEmbedSafe(publicLogChannel, text, "ff0000")
+				utils.sendEmbedSafe(staffLogChannel, text, "ff0000")
+				goto continue
+			end
+			local text = name.." has been automatically unmuted."
+			utils.sendEmbedSafe(publicLogChannel, text, "00ff00", nil, nil, muteUser.mentionString)
+			utils.sendEmbedSafe(staffLogChannel, text, "00ff00")
+
+			-- I know "goto is evil", but it drastically improves code clarity here because Lua doesn't have a continue statement
+			::continue::
 		end
 	end,
 	onEnable = function(self, message, guildSettings, conn)
