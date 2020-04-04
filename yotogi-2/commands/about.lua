13c2004c01674524520562d63e769d0b1c54b7bf
[object Object] <37044997+object-Object@users.noreply.github.com>
2020-04-04 03:21:04 +0000

    change some phrasing

--- yotogi-2/commands/about.lua
+++ yotogi-2/commands/about.lua
@@ -14,7 +14,7 @@ return {
 		message.channel:send{
 			embed = {
 				title = "About Yotogi",
-				description = "• Yotogi is a general-purpose bot with an emphasis on moderation.\n• It is written, hosted, and maintained by [object Object]#0001.\n• Found a bug? Send a private message to Yotogi! Include some information about the bug, like what you were doing when it happened, what you expected to happen, and what actually happened.",
+				description = "• Yotogi is a general-purpose bot with an emphasis on moderation.\n• It is written, hosted, and maintained by [object Object]#0001.\n• Found a bug? Send a direct message to Yotogi! Include some information about the bug, like what you were doing when it happened, what you expected to happen, and what actually happened.",
 				color = discordia.Color.fromHex("00ff00").value,
 				fields = {
 					{name = "Servers", value = #message.client.guilds},
