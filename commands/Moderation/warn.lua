f07d4fab3da4acc4f348e4d56875da4301146d2a
[object Object] <37044997+object-Object@users.noreply.github.com>
2020-07-21 17:23:05 +0000

    localize warn command, it only took a month

--- commands/Moderation/warn.lua
+++ commands/Moderation/warn.lua
@@ -4,8 +4,6 @@ local commandHandler = require("commandHandler")
 
 return {
 	name = "warn",
-	description = "Warn a user.",
-	usage = "warn <ping or id> [| reason]",
 	visible = true,
 	permissions = {"kickMembers","banMembers"},
 	run = function(self, message, argString, args, guildSettings, lang, conn)
@@ -16,7 +14,7 @@ return {
 
 		local warnUser = utils.userFromString(args[1], message.client)
 		if not warnUser then
-			utils.sendEmbed(message.channel, "User "..args[1].." not found.", "ff0000")
+			utils.sendEmbed(message.channel, f(lang.error.user_not_found, args[1]), "ff0000")
 			return
 		end
 		local warnMember = utils.memberFromString(args[1], message.guild)
@@ -24,63 +22,64 @@ return {
 
 		local warnSuccess, warnErr, entry, doKick, doBan = warnUtils.warn(warnMember, warnUser, message.guild, guildSettings, conn)
 		if not warnSuccess then
-			utils.sendEmbed(message.channel, name.." could not be warned because "..warnErr, "ff0000")
+			utils.sendEmbed(message.channel, f(lang.error.warn_fail, name, warnErr), "ff0000")
 			return
 		end
 
 		local reason = argString:match("%|%s+(.+)")
-		reason = reason and " (Reason: "..reason..")" or ""
+		reason = reason and f(lang.g.reason_str, reason) or ""
 
 		local staffLogChannel = guildSettings.staff_log_channel and message.guild:getChannel(guildSettings.staff_log_channel)
+		local responsibleUserStr = f(lang.g.responsible_user_str, utils.name(message.author, message.guild))
 
 		if doKick then
 			local kickValid, kickErr = warnUtils.checkValidKick(warnMember, message.guild)
 			if kickValid then
-				local kickedDM = utils.sendEmbed(warnUser:getPrivateChannel(), "You have been kicked from **"..message.guild.name.."** for reaching "..entry.level.." warning"..utils.s(entry.level).."."..reason, "00ff00")
+				local kickedDM = utils.sendEmbed(warnUser:getPrivateChannel(), f(lang.pl(lang.warn.you_kicked, entry.level), messsage.guild.name, entry.level)..reason, "00ff00")
 				local success, err = warnMember:kick()
 				if not success then
 					-- kick failed, so continue with normal warn messages
 					if kickedDM then kickedDM:delete() end
-					utils.sendEmbed(message.channel, name.." could not be kicked for reaching "..entry.level.." warning"..utils.s(entry.level)..": `"..err.."`. Please report this error to the bot developer by sending Yot a direct message.", "ff0000")
+					utils.sendEmbed(message.channel, f(lang.pl(lang.error.kick_warn_fail, entry.level), name, entry.level, "`"..err.."`")..lang.error.report_error, "ff0000")
 				else
 					-- kick succeeded, exit early
-					local text = name.." has been kicked for reaching "..entry.level.." warning"..utils.s(entry.level).."."..reason
+					local text = f(lang.pl(lang.warn.user_kicked, entry.level), name)..reason
 					utils.sendEmbed(message.channel, text, "00ff00")
-					utils.sendEmbedSafe(staffLogChannel, text, "00ff00", "Responsible user: "..utils.name(message.author, message.guild))
+					utils.sendEmbedSafe(staffLogChannel, text, "00ff00", responsibleUserStr)
 					return
 				end
 			else
 				-- kick invalid, so continue with normal warn messages
-				utils.sendEmbed(message.channel, name.." could not be kicked for reaching "..entry.level.." warning"..utils.s(entry.level).." because "..kickErr, "ff0000")
+				utils.sendEmbed(message.channel, f(lang.pl(lang.error.kick_warn_fail, entry.level), name, entry.level, kickErr), "ff0000")
 			end
 		elseif doBan then
 			local banValid, banErr = warnUtils.checkValidBan(warnMember, message.guild)
 			if banValid then
-				local bannedDM = utils.sendEmbed(warnUser:getPrivateChannel(), "You have been banned from **"..message.guild.name.."** for reaching "..entry.level.." warning"..utils.s(entry.level).."."..reason, "00ff00")
+				local bannedDM = utils.sendEmbed(warnUser:getPrivateChannel(), f(lang.pl(lang.warn.you_banned, entry.level), messsage.guild.name, entry.level)..reason, "00ff00")
 				local success, err = message.guild:banUser(warnUser.id)
 				if not success then
 					-- ban failed, so continue with normal warn messages
 					if bannedDM then bannedDM:delete() end
-					utils.sendEmbed(message.channel, name.." could not be banned for reaching "..entry.level.." warning"..utils.s(entry.level)..": `"..err.."`. Please report this error to the bot developer by sending Yot a direct message.", "ff0000")
+					utils.sendEmbed(message.channel, f(lang.pl(lang.error.ban_warn_fail, entry.level), name, entry.level, "`"..err.."`")..lang.error.report_error, "ff0000")
 				else
 					-- ban succeeded, exit early
-					local text = name.." has been banned for reaching "..entry.level.." warning"..utils.s(entry.level).."."..reason
+					local text = f(lang.pl(lang.warn.user_banned, entry.level), name)..reason
 					utils.sendEmbed(message.channel, text, "00ff00")
-					utils.sendEmbedSafe(staffLogChannel, text, "00ff00", "Responsible user: "..utils.name(message.author, message.guild))
+					utils.sendEmbedSafe(staffLogChannel, text, "00ff00", responsibleUserStr)
 					return
 				end
 			else
 				-- ban invalid, so continue with normal warn messages
-				utils.sendEmbed(message.channel, name.." could not be banned for reaching "..entry.level.." warning"..utils.s(entry.level).." because "..banErr, "ff0000")
+				utils.sendEmbed(message.channel, f(lang.pl(lang.error.ban_warn_fail, entry.level), name, entry.level, banErr), "ff0000")
 			end
 		end
 
 		local warnFooter = commandHandler.strings.warnFooter(guildSettings, entry)
 
-		utils.sendEmbed(warnUser:getPrivateChannel(), "You have been warned in **"..message.guild.name.."**. You now have "..entry.level.." warning"..utils.s(entry.level).."."..reason, "00ff00", warnFooter)
-		local text = name.." has been warned. They now have "..entry.level.." warning"..utils.s(entry.level).."."..reason
+		utils.sendEmbed(warnUser:getPrivateChannel(), f(lang.pl(lang.warn.you_warned, entry.level), message.guild.name, entry.level)..reason, "00ff00", warnFooter)
+		local text = f(lang.pl(lang.warn.user_warned, entry.level), name, entry.level)..reason
 		utils.sendEmbed(message.channel, text, "00ff00", warnFooter)
-		utils.sendEmbedSafe(staffLogChannel, text, "00ff00", "Responsible user: "..utils.name(message.author, message.guild).."\n"..warnFooter)
+		utils.sendEmbedSafe(staffLogChannel, text, "00ff00", responsibleUserStr.."\n"..warnFooter)
 	end,
 	onEnable = function(self, message, guildSettings) -- function called when this command is enabled, return true if enabling can proceed
 		return true
