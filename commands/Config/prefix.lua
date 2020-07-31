4ea3a16bd26082537b5ed53af21c1db164b8e4e5
[object Object] <37044997+object-Object@users.noreply.github.com>
2020-07-31 17:54:38 +0000

    prefix command i18n

--- commands/Config/prefix.lua
+++ commands/Config/prefix.lua
@@ -5,20 +5,18 @@ local options = discordia.storage.options
 
 return {
 	name = "prefix",
-	description = "Show or change the command prefix for Yot in this server.",
-	usage = "prefix [new prefix (may be in an inline code block)]",
 	visible = true,
 	permissions = {"administrator"},
 	run = function(self, message, argString, args, guildSettings, lang, conn)
 		if argString=="" then
-			utils.sendEmbed(message.channel, "Prefix is currently `"..guildSettings.prefix.."`.", "00ff00")
+			utils.sendEmbed(message.channel, f(lang.commands.prefix.prefix_is, guildSettings.prefix), "00ff00")
 			return
 		end
 		local newPrefix = argString:gsub("%`(.+)%`","%1")
 		local stmt = conn:prepare("UPDATE guild_settings SET prefix = ? WHERE guild_id = ?;")
 		stmt:reset():bind(newPrefix, message.guild.id):step()
 		stmt:close()
-		utils.sendEmbed(message.channel,"Prefix updated to `"..newPrefix.."`.","00ff00")
+		utils.sendEmbed(message.channel, f(lang.commands.prefix.prefix_set, guildSettings.prefix, newPrefix),"00ff00")
 	end,
 	onEnable = function(self, message, guildSettings)
 		return true
@@ -30,13 +28,11 @@ return {
 
 		reset = {
 			name = "prefix reset",
-			description = "Reset the command prefix for Yot in this server.",
-			usage = "prefix reset",
 			run = function(self, message, argString, args, guildSettings, lang, conn)
 				local stmt = conn:prepare("UPDATE guild_settings SET prefix = ? WHERE guild_id = ?;")
 				stmt:reset():bind(options.defaultPrefix, message.guild.id):step()
 				stmt:close()
-				utils.sendEmbed(message.channel,"Prefix reset to `"..options.defaultPrefix.."`.","00ff00")
+				utils.sendEmbed(message.channel, f(lang.commands.prefix.prefix_reset, guildSettings.prefix, options.defaultPrefix), "00ff00")
 			end,
 			subcommands = {}
 		}
