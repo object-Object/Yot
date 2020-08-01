48f17ab7537a236c4fc4c737c0deae8c5970ccd2
[object Object] <37044997+object-Object@users.noreply.github.com>
2020-08-01 18:29:31 +0000

    add prefix set command and make prefix command only show prefix

--- commands/Config/prefix.lua
+++ commands/Config/prefix.lua
@@ -6,21 +6,9 @@ local options = discordia.storage.options
 return {
 	name = "prefix",
 	visible = true,
-	permissions = {"administrator"},
+	permissions = {},
 	run = function(self, message, argString, args, guildSettings, lang, conn)
-		if argString=="" then
-			utils.sendEmbed(message.channel, f(lang.commands.prefix.prefix_is, guildSettings.prefix), "00ff00")
-			return
-		end
-		local newPrefix = argString:gsub("%`(.+)%`","%1")
-		if guildSettings.prefix==newPrefix then
-			utils.sendEmbed(message.channel, f(lang.error.prefix_already_set, newPrefix),"ff0000")
-			return
-		end
-		local stmt = conn:prepare("UPDATE guild_settings SET prefix = ? WHERE guild_id = ?;")
-		stmt:reset():bind(newPrefix, message.guild.id):step()
-		stmt:close()
-		utils.sendEmbed(message.channel, f(lang.commands.prefix.prefix_set, guildSettings.prefix, newPrefix),"00ff00")
+		utils.sendEmbed(message.channel, f(lang.commands.prefix.prefix_is, guildSettings.prefix), "00ff00")
 	end,
 	onEnable = function(self, message, guildSettings)
 		return true
@@ -30,8 +18,30 @@ return {
 	end,
 	subcommands = {
 
+		set = {
+			name = "prefix set",
+			permissions = {"administrator"},
+			run = function(self, message, argString, args, guildSettings, lang, conn)
+				if argString=="" then
+					commandHandler.sendUsage(message.channel, guildSettings, lang, self)
+					return
+				end
+				local newPrefix = argString:gsub("%`(.+)%`","%1")
+				if guildSettings.prefix==newPrefix then
+					utils.sendEmbed(message.channel, f(lang.error.prefix_already_set, newPrefix),"ff0000")
+					return
+				end
+				local stmt = conn:prepare("UPDATE guild_settings SET prefix = ? WHERE guild_id = ?;")
+				stmt:reset():bind(newPrefix, message.guild.id):step()
+				stmt:close()
+				utils.sendEmbed(message.channel, f(lang.commands.prefix.prefix_set, guildSettings.prefix, newPrefix),"00ff00")
+			end,
+			subcommands = {}
+		},
+
 		reset = {
 			name = "prefix reset",
+			permissions = {"administrator"},
 			run = function(self, message, argString, args, guildSettings, lang, conn)
 				if guildSettings.prefix==options.defaultPrefix then
 					utils.sendEmbed(message.channel, f(lang.error.prefix_already_set, options.defaultPrefix),"ff0000")
